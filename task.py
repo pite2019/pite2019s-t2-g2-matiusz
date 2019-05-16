6be32ce41103af1cff2722e7a9c8bac27e0f61df
matiusz <37378034+matiusz@users.noreply.github.com>
2019-05-16 14:56:40 +0200

    7 unittests for banking module

--- task.py
+++ task.py
@@ -1,4 +1,50 @@
 
+import unittest
+
+class TestBank(unittest.TestCase):
+    def test_add_client(self):
+        bank = Bank("testbank")
+        bank.add_client("testclient")
+        self.assertEqual(bank.ClientList[0].Name, "testclient")
+    def test_remove_client(self):
+        bank = Bank("testbank")
+        client = bank.add_client("testclient")
+        bank.remove_client(client)
+        self.assertEqual(bank.ClientList, [])
+
+class TestClient(unittest.TestCase):
+    def test_money_input(self):
+        client = Client("testclient")
+        client.Money=100
+        client.money_input(42)
+        self.assertEqual(client.Money, 142)
+    def test_money_withdrawal(self):
+        client = Client("testclient")
+        client.Money=100
+        client.money_withdrawal(42)
+        self.assertEqual(client.Money, 58)
+    def test_failed_withdrawal(self):
+        client = Client("testclient")
+        client.Money=100
+        self.assertEqual(client.money_withdrawal(142), 0)
+        self.assertEqual(client.Money, 100)
+    def test_transaction(self):
+        client1 = Client("testclient1")
+        client2 = Client("testclient2")
+        client1.Money=100
+        client2.Money=50
+        client1.transaction(client2, 60)
+        self.assertEqual(client1.Money, 40)
+        self.assertEqual(client2.Money, 110)
+    def test_failed_transaction(self):
+        client1 = Client("testclient1")
+        client2 = Client("testclient2")
+        client1.Money=50
+        client2.Money=50
+        self.assertEqual(client1.transaction(client2, 60), 0)
+
+
+
 
 class Bank:
     def __init__(self, name):
@@ -12,18 +58,17 @@ class Bank:
         self.ClientList.remove(Client)
 
 
-
-
 class Client:
     def __init__(self, Name):
         self.Name = Name
         self.Money = 0
         self.Log = []
+        self.MaxCredit = 0
     def money_input(self, Amount):
         self.Money = self.Money + Amount
         self.Log.append({"input", Amount})
     def money_withdrawal(self, Amount):
-        if self.Money<Amount:
+        if self.Money+self.MaxCredit<Amount:
             self.Log.append({"withdrawal attempt, not enough money", Amount})
             return 0
         else:
@@ -31,7 +76,7 @@ class Client:
             self.Money =  self.Money-Amount
             return Amount
     def transaction(self, Client, Amount):
-        if self.Money<Amount:
+        if self.Money+self.MaxCredit<Amount:
             self.Log.append({"transaction failed, not enough money", Amount})
             return 0
         else:
@@ -40,18 +85,8 @@ class Client:
             self.Log.append({"transaction from " + self.Name + " to " + Client.Name, Amount})
             Client.Log.append({"transaction from " + self.Name + " to " + Client.Name, Amount})
             return Amount
+    def __str__(self):
+        return self.Name +" " + str(self.Money) + " " + str(self.Log)
 
-def Main():
-    bank1 = Bank("Polski")
-    client1 = bank1.add_client("kowalski")
-    client2 = bank1.add_client("nowak")
-    client1.money_input(100)
-    client1.money_withdrawal(50)
-    client1.money_withdrawal(100)
-    client2.money_input(200)
-    client2.transaction(client1, 150)
-    print(client1.Log)
-    print(client2.Log)
-
-Main()
-
+if __name__ == "__main__" :
+    unittest.main()
